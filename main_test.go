090e77414099821ad9f3febf6f52b29465cd01c3
febrina <febrina.andayani@tanihub.com>
2021-02-17 00:29:56 +0700

    menambah func TestCreaeEndPoint

--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,4 +20,18 @@ func TestSubtract(t *testing.T) {
 	total := Subtract(1, 3)
 	assert.NotNil(t, total, "The `total` should not be `nil`")
 	assert.Equal(t, -2, total, "Expecting `-2`")
+}
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/create", CreateEndpoint).Methods("GET")
+	return router
+}
+
+func TestCreateEndpoint(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/create", nil)
+	response := httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+	fmt.Println(response.Body)
 }
